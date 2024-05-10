f3a841c5e86bbc0d162ae623a7abd39a7ed1bccc
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2024-05-10 11:51:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     name='autopeptideml',
     packages=find_packages(exclude=['examples']),
     url='https://ibm.github.io/AutoPeptideML/',
-    version='0.2.10',
+    version='0.2.12',
     zip_safe=False,
 )
