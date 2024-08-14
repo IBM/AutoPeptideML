1e6c5fe96940956ff4b64b8fb4c1811fc32f6a8d
Raúl Fernández Díaz <raulfd@ibm.com>
2024-08-14 21:50:14 +0100

    🛠️ Code-dev: Prepare setup for v.0.3.2

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     name='autopeptideml',
     packages=find_packages(exclude=['examples']),
     url='https://ibm.github.io/AutoPeptideML/',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
