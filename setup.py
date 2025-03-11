c61dba3409b119834fcbac9ccbb3dded8d826ada
RaulFD-creator <raulfdz9@gmail.com>
2025-03-11 08:00:14 +0000

    🚀 v.1.0.6

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     name='autopeptideml',
     packages=find_packages(exclude=['examples']),
     url='https://ibm.github.io/AutoPeptideML/',
-    version='1.0.5',
+    version='1.0.6',
     zip_safe=False,
 )
